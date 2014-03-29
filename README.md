4f4def5eedbfdba8d5badff6be4a44b922a04f73
Ferran Pons <ferranpons@gmail.com>
2014-03-29 13:06:46 +0100

    Added Require.js to the Tests
    After adding Require to the main project we needed to do the same with
    the Tests.

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Rescue Me
 A WebGL game experiment using Three.js, Physijs, and other libs.
 
 
-Personal Project. 
+Personal Project
 -------
 The main Objective of this project is learn 3D through a little video game, that will include almost all the techniques used in the industry. Like physics, in-game cinematics, animated characters, dialogues, GUI, etc.
+
 
 
 **Work in progress...**
---
+++ tests/mainTests.js
@@ -0,0 +1,61 @@
+"use strict";
+require.config({
+	baseUrl: 'js/',
+    paths: {
+        QUnit: '../lib/qunit-1.14.0',
+        jquery: '../../js/libs/jquery-1.10.2.min',
+        three: '../../js/libs/three',
+        threeCore: '../../js/libs/three.min',
+        physijs: '../../js/libs/physi',
+        stats: '../../js/libs/stats.min',
+        objectLoader: '../../js/libs/ObjectLoader',
+        pointerLockControls: '../../js/libs/PointerLockControls',
+
+        conversor: '../../js/game/conversor',
+        game: '../../js/game/game',
+        world: '../../js/game/world',
+        player: '../../js/game/player',
+        enemy: '../../js/game/enemy',
+        meshes: '../../js/game/meshes',
+        bullet: '../../js/game/bullet',
+
+        blocker: '../../js/generic/blocker',
+        instructions: '../../js/generic/instructions',
+        pointerlockchange: '../../js/generic/pointerlockchange',
+        pointerlockerror: '../../js/generic/pointerlockerror'
+    },
+    shim: {
+        'QUnit': {
+            exports: 'QUnit',
+            init: function() {
+                QUnit.config.autoload = false;
+                QUnit.config.autostart = false;
+           }
+        },
+        'threeCore': { exports: 'THREE' },
+        'stats': { exports: 'Stats' },
+        'physijs': { exports: 'Physijs' },
+        'pointerLockControls': { exports: 'PointerLockControls' },
+
+        'conversor': { exports: 'CONVERSOR' },
+        'game': { exports: 'GAME' },
+        'world': { exports: 'WORLD' },
+        'player': { exports: 'Player' },
+        'enemy': { exports: 'Enemy' },
+        'meshes': { exports: 'MESHES' },
+        'bullet': { exports: 'BULLET' },
+
+        'blocker': { exports: 'blocker' },
+        'instructions': { exports: 'instructions' },
+        'pointerlockchange': { exports: 'pointerlockchange' },
+        'pointerlockerror': { exports: 'pointerlockerror' } 
+    }
+});
+
+require(["QUnit", "three", "playerTests", "enemyTests"],
+	function(QUnit, THREE, playerTests, enemyTests) {
+
+        QUnit.load();
+        QUnit.start();
+    }
+);
